cf9d6dc6c17325bcae4920be776f4f720346e957
Samuel Odigie <samodigie98@gmail.com>
2023-01-11 23:57:32 +0000

    completed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,7 @@ import CardItem from "./CardItem";
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out these EPIC Products!</h1>
+      <h1>Latest drops and upcoming design</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
